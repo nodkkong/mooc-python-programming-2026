778dc80e7b37b427910c0a6711079898240641c8
nodkkong <kimnoah15@gmail.com>
2026-08-23 22:54:38 -0700

    Daily commit: 2026-08-23

--- part08-14_clock/src/clock.py
+++ part08-14_clock/src/clock.py
@@ -1,1 +1,25 @@
 # Write your solution here:
+class Clock:
+    def __init__(self, hours: int, minutes: int, seconds: int):
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
+
+    def tick(self):
+        self.seconds += 1
+        if self.seconds > 59:
+            self.seconds = 0
+            self.minutes += 1
+            if self.minutes > 59:
+                self.minutes = 0
+                self.hours += 1
+                if self.hours > 23:
+                    self.hours = 0
+
+    def set(self, hours: int, minutes: int):
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = 0
+
+    def __str__(self):
+        return f"{self.hours:02d}:{self.minutes:02d}:{self.seconds:02d}"
