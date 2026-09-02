e9d30779e0d446d11382d346638b91650b17c2a7
nodkkong <kimnoah15@gmail.com>
2026-09-02 00:09:13 -0700

    Daily commit: 2026-09-01 | Scope of methods & Class attributes

--- part09-12_service_charge/src/service_charge.py
+++ part09-12_service_charge/src/service_charge.py
@@ -1,3 +1,24 @@
 # WRITE YOUR SOLUTION HERE:
+class BankAccount:
+    def __init__(self, name: str, account_number: str, balance: float):
+        self.__name = name
+        self.__account_number = account_number
+        self.__balance = balance
 
+    def __service_charge(self):
+        if self.__balance > 0:
+            self.__balance *= 0.99
 
+    def deposit(self, amount: float):
+        if amount > 0:
+            self.__balance += amount
+            self.__service_charge()
+
+    def withdraw(self, amount: float):
+        if self.__balance >= amount:
+            self.__balance -= amount
+            self.__service_charge()
+
+    @property
+    def balance(self):
+        return self.__balance
--- part09-14_list_helper/src/list_helper.py
+++ part09-14_list_helper/src/list_helper.py
@@ -1,1 +1,29 @@
 # WRITE YOUR SOLUTION HERE:
+class ListHelper:
+    @classmethod
+    def greatest_frequency(cls, my_list: list):
+        counts = {}
+        for item in my_list:
+            if item not in counts:
+                counts[item] = 0
+            counts[item] += 1
+        most_common = None
+        highest_count = 0
+        for item, count in counts.items():
+            if count > highest_count:
+                highest_count = count
+                most_common = item
+        return most_common
+
+    @classmethod
+    def doubles(cls, my_list: list):
+        counts = {}
+        for item in my_list:
+            if item not in counts:
+                counts[item] = 0
+            counts[item] += 1
+        double_count = 0
+        for item in counts:
+            if counts[item] >= 2:
+                double_count += 1
+        return double_count
